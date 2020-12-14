0463fcf931090c33a128b35722b0ff2e0c03b3fb
Shuliak Leonid <zeleny777@gmail.com>
2020-12-15 00:32:13 +0300

    feat: promise task

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -57,8 +57,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -80,8 +80,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
