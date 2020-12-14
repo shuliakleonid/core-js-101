50b784105cbfdfad4b5a27c4d6fe7666a35953d7
Shuliak Leonid <zeleny777@gmail.com>
2020-12-15 00:56:59 +0300

    faet: end task

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -371,8 +371,8 @@ function isBracketsBalanced(str) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  return parseInt(num, 10).toString(n);
 }
 
 
